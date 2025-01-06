25dab3ef25dcc3de7fbf91461c91a82f3a7f8734
Shatanshu <shatanshu.1923csi1044@kiet.edu>
2025-01-06 22:14:53 +0530

    Commit for Jan 5

--- src/main/java/com/microservice/rest/webservices/restful_webservices/exception/CustomizedResponseEntityExceptionHandler.java
+++ src/main/java/com/microservice/rest/webservices/restful_webservices/exception/CustomizedResponseEntityExceptionHandler.java
@@ -2,13 +2,17 @@ package com.microservice.rest.webservices.restful_webservices.exception;
 
 import java.time.LocalDate;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.microservice.rest.webservices.restful_webservices.user.UserNotFoundException;
 
 @ControllerAdvice
 public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
@@ -16,10 +20,23 @@ public class CustomizedResponseEntityExceptionHandler extends ResponseEntityExce
 	
 
 	@ExceptionHandler(Exception.class)
-	public final ResponseEntity<Object> handleExceptions(Exception ex, WebRequest request) throws Exception {
+	public final ResponseEntity<ErrorDetails> handleAllExceptions(Exception ex, WebRequest request) throws Exception {
 		ErrorDetails errorDetails = new ErrorDetails(LocalDate.now(), ex.getMessage(), request.getDescription(false));
-		return new ResponseEntity(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
+		return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.INTERNAL_SERVER_ERROR);
 
+	}
+	
+	@ExceptionHandler(UserNotFoundException.class)
+	public final ResponseEntity<ErrorDetails> handleUserNotFoundException(Exception ex, WebRequest request) throws Exception {
+		ErrorDetails errorDetails = new ErrorDetails(LocalDate.now(), ex.getMessage(), request.getDescription(false));
+		return new ResponseEntity<ErrorDetails>(errorDetails, HttpStatus.NOT_FOUND);
+
+	}
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(
+			MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+		ErrorDetails errorDetails = new ErrorDetails(LocalDate.now(), ex.getMessage(), request.getDescription(false));
+		return new ResponseEntity(errorDetails, HttpStatus.BAD_REQUEST);
 	}
 
 }
--- src/main/java/com/microservice/rest/webservices/restful_webservices/user/User.java
+++ src/main/java/com/microservice/rest/webservices/restful_webservices/user/User.java
@@ -2,10 +2,16 @@ package com.microservice.rest.webservices.restful_webservices.user;
 
 import java.time.LocalDate;
 
+import jakarta.validation.constraints.Past;
+import jakarta.validation.constraints.Size;
+
 public class User {
 	private int id;
+	@Size(min = 2, message="Name should have atleast 2 characters")
 	private String name;
+	@Past(message="Birth Date should be in the past")
 	private LocalDate birthDate;
+
 	@Override
 	public String toString() {
 		return "User [id=" + id + ", name=" + name + ", birthDate=" + birthDate + "]";
@@ -21,20 +27,25 @@ public class User {
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public LocalDate getBirthDate() {
 		return birthDate;
 	}
+
 	public void setBirthDate(LocalDate birthDate) {
 		this.birthDate = birthDate;
 	}
-	
+
 }
