bad9b3ecadad3fb7f268cabc3bd7292123fcab6e
Shatanshu <shatanshu.1923csi1044@kiet.edu>
2025-01-22 23:36:00 +0530

    commiting changes

---
+++ spring-cloud-config-server/src/test/java/com/microservices/limits_service/spring_cloud_config_server/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservices.limits_service.spring_cloud_config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
