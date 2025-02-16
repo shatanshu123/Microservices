d68e6f3ab5973b5b5e9e56bfcea35633ec460e34
Shatanshu <shatanshu.1923csi1044@kiet.edu>
2025-02-16 22:49:48 +0530

    changes

--- currency-exchange-service/src/main/java/com/microservices/currency_exchange_service/CurrencyExchangeController.java
+++ currency-exchange-service/src/main/java/com/microservices/currency_exchange_service/CurrencyExchangeController.java
@@ -1,15 +1,17 @@
 package com.microservices.currency_exchange_service;
 
-import java.math.BigDecimal;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+
 @RestController
 public class CurrencyExchangeController {
+	private Logger logger = LoggerFactory.getLogger(CurrencyExchangeController.class);
 	@Autowired
 	private CurrencyExchangeRepository currencyExchangeRepository;
 	@Autowired
@@ -17,6 +19,7 @@ public class CurrencyExchangeController {
 
 	@GetMapping("/currency-exchange/from/{from}/to/{to}")
 	public CurrencyExchange retrieveExchangeValue(@PathVariable String from, @PathVariable String to) throws Exception {
+		logger.info("retrieveExchangeValue called with {} to {}", from, to);
 		CurrencyExchange currencyExchange=currencyExchangeRepository.findByFromAndTo(from, to);
 		if(currencyExchange==null) {
 			throw new Exception("Unable to find data for"+ from + " to "+to);
