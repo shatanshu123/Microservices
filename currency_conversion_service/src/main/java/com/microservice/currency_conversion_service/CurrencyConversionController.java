7f0a8a84a7c2cd297c196bac514dad152c60c2a6
Shatanshu <shatanshu.1923csi1044@kiet.edu>
2025-04-26 21:10:44 +0530

    latest code

--- currency_conversion_service/src/main/java/com/microservice/currency_conversion_service/CurrencyConversionController.java
+++ currency_conversion_service/src/main/java/com/microservice/currency_conversion_service/CurrencyConversionController.java
@@ -4,16 +4,31 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+@Configuration(proxyBeanMethods = false)
+class RestTemplateConfiguration {
+
+	@Bean
+	RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+}
+
 @RestController
 public class CurrencyConversionController {
 	@Autowired
 	private CurrencyExchangeProxy proxy;
+
+	@Autowired
+	private RestTemplate restTemplate;
 
 	@GetMapping("/currency-conversion/from/{from}/to/{to}/quantity/{quantity}")
 	public CurrencyConversion calculateCurrencyConversion(@PathVariable String from, @PathVariable String to,
@@ -22,7 +37,7 @@ public class CurrencyConversionController {
 		HashMap<String, String> UriVariables = new HashMap<>();
 		UriVariables.put("from", from);
 		UriVariables.put("to", to);
-		ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity(
+		ResponseEntity<CurrencyConversion> responseEntity = restTemplate.getForEntity(
 				"http://localhost:8000/currency-exchange/from/{from}/to/{to}", CurrencyConversion.class, UriVariables);
 		CurrencyConversion currencyConversion = responseEntity.getBody();
 		return new CurrencyConversion(currencyConversion.getId(), from, to, quantity,
